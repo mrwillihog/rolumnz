ad84d56e510944f5e3cd80453a0cebae6395f905
Matthew Williams <m.williams@me.com>
2013-05-20 16:41:04 +0100

    master Initial commit

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+firesass = true
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "style"
+sass_dir = "sass"
+images_dir = "sass/themes/sprites"
+javascripts_dir = "script"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style  = :expanded
+line_comments = true
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+sass_options = {:debug_info => true}
